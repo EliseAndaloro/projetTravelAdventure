1f2518bdcf448a9e1b72aced0b9adfc9d57a0723
Amelie <amelie.ar98@gmail.com>
2019-04-01 10:40:21 +0200

    navbar

---
+++ resources/js/script.js
@@ -0,0 +1,5 @@
+var instance = M.Tabs.init(el, options);
+var instance = M.Tabs.getInstance(elem);
+instance.select('tab_id');
+instance.updateTabIndicator();
+instance.destroy();
--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -5,11 +5,12 @@
         <title>App Name - @yield('title')</title>
         <link rel="stylesheet" href=" {{ asset('css/app.css') }} ">
         <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+ <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css">
         <link rel="stylesheet" href="css/style.css">
     </head>
     <body>
         <header>
-		<nav class="nav nav-pills nav-fill">
+		<nav class="header nav nav-pills nav-fill">
   			<a class="nav-item nav-link " href="#">ACCUEIL</a>
   			<a class="nav-item nav-link" href="#">SEJOUR</a>
   			<a class="nav-item nav-link" href="#">WEEK-END</a>
@@ -18,14 +19,19 @@
   			<a class="nav-item nav-link" href="#">CONTACT</a>
   			<a class="nav-item nav-link" href="#"><i class="small material-icons">card_travel</i></a>
 		</nav>
+
 	</header>
-        
+
         <div>
             @yield('content')
         </div>
-        
+
+
+
+        <script type="text/javascript" src="js/materialize.min.js"></script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/js/materialize.min.js"></script>
         <script type="text/javascript" src=" {{ asset('js/app.js') }} "></script>
-        
+        <script type="text/javascript" src="js/script.js"></script>
     </body>
     <footer>
     	FOOTER
