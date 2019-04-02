5f73d2649b5d35c44921350549ff82a1213ac1aa
Unknown <elise.andaloro@hotmail.fr>
2019-04-02 10:04:09 +0200

    z-index + bouton

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -8,11 +8,13 @@
 		<div id="mongolfiere2">
 
 		</div>
+		
+		<a class="waves-effect waves-light btn" id="button">Voir les offres !</a>
 	</div>
 
 
 	<div id="slide2">
-
+		<a class="waves-effect waves-light btn" id="button2">Voir les offres !</a>
 	</div>
 	<div id="slide3">
 
