95b67cdb26814a28013243a1531d4db633847242
Amelie <amelie.ar98@gmail.com>
2019-04-03 15:27:48 +0200

    compte connexion fini

--- resources/views/compte.blade.php
+++ resources/views/compte.blade.php
@@ -1,20 +1,21 @@
 @extends ('layout')
 
 @section ('content')
+	<link rel="stylesheet" href="css/compte.css">
 	<img src="img/connexion.svg" style="margin-top : 100px; width: 300px; height: 250px;
-	margin-left: 38%; margin-right: 38%;">
+	margin-left: 39%; margin-right: 39%;">
 	<div class="container">
     <div class="row justify-content-center">
         <div class="col-md-8">
             <div class="card">
-                <div class="card-header">{{ __('Login') }}</div>
+                <div class="card-header">- connecte toi -</div>
 
                 <div class="card-body">
                     <form method="POST" action="{{ route('login') }}">
                         @csrf
 
                         <div class="form-group row">
-                            <label for="email" class="col-md-4 col-form-label text-md-right">{{ __('E-Mail Address') }}</label>
+                            <label for="email" class="col-md-4 col-form-label text-md-right">Adresse mail</label>
 
                             <div class="col-md-6">
                                 <input id="email" type="email" class="form-control{{ $errors->has('email') ? ' is-invalid' : '' }}" name="email" value="{{ old('email') }}" required autofocus>
@@ -28,7 +29,7 @@
                         </div>
 
                         <div class="form-group row">
-                            <label for="password" class="col-md-4 col-form-label text-md-right">{{ __('Password') }}</label>
+                            <label for="password" class="col-md-4 col-form-label text-md-right">Mot de passe</label>
 
                             <div class="col-md-6">
                                 <input id="password" type="password" class="form-control{{ $errors->has('password') ? ' is-invalid' : '' }}" name="password" required>
@@ -40,30 +41,14 @@
                                 @endif
                             </div>
                         </div>
-
-                        <div class="form-group row">
-                            <div class="col-md-6 offset-md-4">
-                                <div class="form-check">
-                                    <input class="form-check-input" type="checkbox" name="remember" id="remember" {{ old('remember') ? 'checked' : '' }}>
-
-                                    <label class="form-check-label" for="remember">
-                                        {{ __('Remember Me') }}
-                                    </label>
-                                </div>
-                            </div>
-                        </div>
-
-                        <div class="form-group row mb-0">
-                            <div class="col-md-8 offset-md-4">
-                                <button type="submit" class="btn btn-primary">
-                                    {{ __('Login') }}
-                                </button>
-
-                                @if (Route::has('password.request'))
-                                    <a class="btn btn-link" href="{{ route('password.request') }}">
-                                        {{ __('Forgot Your Password?') }}
-                                    </a>
-                                @endif
+												<div class="mesBoutons">
+												<button type="submit" class="login" style="margin-bottom: 15px;">
+                          {{ __('Login') }}
+                        </button>
+												<button class="login">
+												<a href="inscription">Je m'inscris</a>
+												</button>
+												</div>
                             </div>
                         </div>
                     </form>
@@ -72,7 +57,7 @@
         </div>
     </div>
 </div>
-<a href="inscription">Je m\'inscris !</a>
+
 
 
 @endsection
