f4457ed301f159c2f37efa76095a2deab06118fa
Amelie <amelie.ar98@gmail.com>
2019-04-04 16:27:45 +0200

    juste un details

--- resources/views/detailsproduits.blade.php
+++ resources/views/detailsproduits.blade.php
@@ -7,13 +7,13 @@
 <div class="contenu">
 
 	<div class="row" style="z-index:2;">
-
+	@foreach($product as $product)
 		<div class="col s1"id="separateur">
 			<img class="materialboxed" width="450" src="{{ $product->img }}">
 		</div>
 
 		<div class="col s1">
-		@foreach($product as $product)
+
 			<h5 class="font1">{{ $product->name }}</h5>
 			<div id="price">{{ $product->price }} €</div>
 			<p class="font">{{ $product->description }}
