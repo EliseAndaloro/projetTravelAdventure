aa20717cf4caac8bb8e3beaa552a8475ec3cf104
Unknown <elise.andaloro@hotmail.fr>
2019-04-04 09:22:21 +0200

    mise en place 2eme image page accueil

--- resources/views/categorie.blade.php
+++ resources/views/categorie.blade.php
@@ -1,26 +1,42 @@
-@extends ('layout')
+@extends ('layout') 
 
 @section ('content')
 <link rel="stylesheet" href="css/categorie.css">
-@foreach($products as $product)
+
+
+<div class="container">
 <div class="row">
-  <div class="col s3 offset-s3">
-		<div class="contenu">
-			<div class="card">
-		    <div class="card-image waves-effect waves-block waves-light">
-		      <img class="activator" src="img/desertTravel.jpg">
-		    </div>
-		    <div class="card-content">
-		      <span class="card-title activator grey-text text-darken-4">{{ $product->name }}<i class="material-icons right">more_vert</i></span>
-		      <p><a href="#">En savoir plus</a></p>
-		    </div>
-		    <div class="card-reveal">
-		      <span class="card-title grey-text text-darken-4">{{ $product->name }}<i class="material-icons right">close</i></span>
-		      <p>{{ $product->description }} </p>
-		    </div>
-		  </div>
-		</div>
+
+	<div class="col s3">coucou</div>
+	@foreach($products as $product)
+	<div class="col s4 ">
+
+		
+			<div class="card" id="carte">
+				<div class="card-image waves-effect waves-block waves-light">
+					<img class="activator" src="img/desertTravel.jpg">
+				</div>
+				<div class="card-content">
+					<span class="card-title activator grey-text text-darken-4">{{
+						$product->name }}<i class="material-icons right">more_vert</i>
+					</span>
+					<p>
+						<a href="#">En savoir plus</a>
+					</p>
+				</div>
+				<div class="card-reveal">
+					<span class="card-title grey-text text-darken-4">{{ $product->name
+						}}<i class="material-icons right">close</i>
+					</span>
+					<p>{{ $product->description }}</p>
+				</div>
+			</div>
+		
+
 	</div>
 @endforeach
+</div>
 
-@endsection
+</div>
+
+ @endsection
