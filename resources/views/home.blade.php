4732bfa2ceb5c3995b038f2bf836ecde85f3bb94
Unknown <elise.andaloro@hotmail.fr>
2019-04-09 11:40:24 +0200

    Fond page d'accueil + footer

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -7,19 +7,18 @@
 	<div id="mongolfiere2"></div>
 
 	<a class="waves-effect waves-light btn" id="button">voir les randonnées</a>
-	<div class="textslide1">
-	<h3 class="titleslide1">LONG FOREST ADVENTURE</h3>
-	<p class="text"> <em>"L'unique moyen de savoir jusqu'où l'on peut aller, 
-		c'est de se mettre en route et de marcher." Henri Bergson</em></p>
-	</div>
+			
 </div>
 
 
 <div id="slide2">
-  <p class="textWeek">hello summer</p>
+
 	<a class="waves-effect waves-light btn" id="button2">voir les weekend</a>
+	
 </div>
 
-<div id="slide3"></div>
+<div id="slide3">
+
+	</div>
 
 @endsection
