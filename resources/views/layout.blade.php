a52f3b8b07ddab2b5a350a848145d6e3d272bb46
Unknown <elise.andaloro@hotmail.fr>
2019-04-10 12:12:45 +0200

    position logout

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -27,11 +27,11 @@
                                     {{ Auth::user()->name }} <span class="caret"></span>
                                 </a>
 
-                                <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
-                                    <a class="dropdown-item" href="{{ route('logout') }}"
+                                <div class="dropdown-menu dropdown-menu-right" id="deconnexion" aria-labelledby="navbarDropdown">
+                                    <a class="dropdown-item" id="deco" href="{{ route('logout') }}"
                                        onclick="event.preventDefault();
                                                      document.getElementById('logout-form').submit();">
-                                        {{ __('Logout') }}
+                                        {{ __('Se Deconnecter') }}
                                     </a>
 
                                     <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
