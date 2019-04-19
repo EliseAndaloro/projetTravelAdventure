3f03021cf668ebdc0d0213117f8cb47d9dbf77a8
Amelie <amelie.ar98@gmail.com>
2019-04-19 17:42:15 +0200

    ça ne marche pas :c

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -2,12 +2,12 @@
 <html>
     <head>
         <meta charset="UTF-8">
-        <meta name="viewport" content="width=device-width" />
+        <meta name=”viewport” content=”width=device-width, initial-scale=1”>
         <title>Travel Adventure</title>
         <link rel="stylesheet" href=" {{ asset('css/app.css') }} ">
         <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
  		<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css">
-        <link rel="stylesheet" href="{{ asset('css/style.css')}}">
+        <link rel="stylesheet" href="{{ asset('css/style.css')}}" media="screen">
         <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
     </head>
     <body>
