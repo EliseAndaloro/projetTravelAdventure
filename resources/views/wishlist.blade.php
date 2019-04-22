3da48a685582b0a7f7c9652b087ec338a19c2c75
Amelie <amelie.ar98@gmail.com>
2019-04-22 12:30:21 +0200

    wishlist Ok

--- resources/views/wishlist.blade.php
+++ resources/views/wishlist.blade.php
@@ -2,11 +2,11 @@
 
 @section ('content')
 <link rel="stylesheet" href="{{asset('css/wish.css')}}">
-<div class="card">
+<div class="cardWish card">
 
 
 @foreach($wishlist as $wishlist )
-<div class="row">
+<div class="cardWish2 row">
   <div class="col s6">
     <img class="imgWish" src="{{ asset($wishlist->img) }}">
   </div>
@@ -22,14 +22,6 @@
     </div>
   </div>
 </div>
-<form class="form-horizontal" method="POST"
-  action="{{ route('wishlist.destroy', '$wishlist->wish_id') }}">
-  {{ csrf_field() }} {{ method_field('DELETE') }}
-  <a class="btn btn-primary" data-toggle="collapse"
-    href="#collapseExample" role="button" aria-expanded="false"
-    aria-controls="collapseExample"><i class="small material-icons">delete</i></a>
-
-</form>
 
 @endforeach
 
